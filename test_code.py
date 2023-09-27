368c07789a9202997b58a082d7a35df3426a44ee
supriya2406 <141736394+supriya2406@users.noreply.github.com>
2023-09-27 23:09:31 +0530

    Add files via upload

---
+++ test_code.py
@@ -0,0 +1,15 @@
+list_1=[1,2,3,4,5]
+list_1.reverse()
+print("The reversed list",list_1)
+
+list_2=[1,2,3,4,5,6]
+list_3=list_2.copy()
+print(list_3)
+
+list_4=[5,58,9,42,457]
+list_4.sort()
+print("The sorted list",list_4)
+
+list_5=[1,2,4,5,6,7,8]
+index_slicing=list_5[2:]
+print(index_slicing)
